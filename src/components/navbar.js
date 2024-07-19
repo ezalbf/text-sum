46babebb983f1862c079f9055ba6ff45f817bb62
niko <nikonartey@gmail.com>
2024-07-19 11:57:47 +0000

    included navbar icons

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,16 @@ import '../css/navbar.css'
 
 function navbar() {
   return (
-    <nav>
-      <div>
-        
-      </div>
-      <div>
+      <nav class="navbar">
 
-      </div>
+        <div class="logo">Logo</div>
+
+        <div class="user-icon">
+          <lord-icon
+            src="https://cdn.lordicon.com/kthelypq.json"
+            trigger="hover">
+          </lord-icon>
+          </div>
     </nav>
   )
 }
